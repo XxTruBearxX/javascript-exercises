7043e3f017f2d6a9e8cd638c1882d7654385fae2
XxTruBearxX <98701856+XxTruBearxX@users.noreply.github.com>
2025-11-29 17:56:47 -0700

    completed assignment for reversing the strings

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  // Convert the string into an array of characters
+  const chararray = word.split('');
+  // Reverse the array of characters
+  const reversedArray = chararray.reverse();
+  // Join the reversed array back into a string
+  const answer = reversedArray.join('');
+  return answer;
 
 };
 
