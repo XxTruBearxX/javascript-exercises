90fa57455e4ddc0f3d92542fc8eb5323df19c6f3
XxTruBearxX <98701856+XxTruBearxX@users.noreply.github.com>
2025-11-29 18:37:22 -0700

    completed assignment for sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num, num2) {
+  // validate inputs
+  if (
+    typeof num !== 'number' ||
+    typeof num2 !== 'number' ||
+    !Number.isInteger(num) ||
+    !Number.isInteger(num2) ||
+    num < 0 ||
+    num2 < 0
+  ) {
+    return 'ERROR';
+  }
+  const start = Math.min(num, num2);
+  const end = Math.max(num, num2);
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }  
+  return sum;
 };
 
 // Do not edit below this line
